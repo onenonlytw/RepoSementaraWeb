5081a12f0282f4e57b1f5a2fbe5bf554a45a662d
AdnanFauzanR <adnanfauzanr29@gmail.com>
2022-12-15 21:42:41 +0800

    Tugas Web

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const { PORT, mongoUri } = require('./node_modules/config')
+const cors = require('cors')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+require('dotenv').config();
+
+app.use(cors()) // to allow cross origin requests
+app.use(bodyParser.json()) // to convert the request into JSON
+
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+
+    })
+    .then(() => console.log('MongoDB database Connected...'))
+    .catch((err) => console.log(err))
+
+app.listen(process.env.PORT, () => console.log(`App listening at http://localhost:${process.env.PORT}`))
